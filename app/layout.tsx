22cae27dbb5bd2238b3fe214d0a5753e638bd561
shhanifff <shanifsha12356@gmail.com>
2026-02-20 11:14:13 +0530

    add google analytics code in app/layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -57,6 +58,18 @@ export default function RootLayout({
           inter.variable,
         )}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QNLGYGSCJT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QNLGYGSCJT');
+          `}
+        </Script>
         {children}
       </body>
     </html>
